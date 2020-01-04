b19b8a8aaf0ddf9a300daf6f88015e57667f9e03
Jonathan Sexton <joncsexton@.com>
2020-01-03 21:46:05 -0600

    moved bottom nav links code to correct files

--- src/app/components/footer/footer-links/footer-links.component.ts
+++ src/app/components/footer/footer-links/footer-links.component.ts
@@ -6,63 +6,6 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./footer-links.component.css']
 })
 export class FooterLinksComponent implements OnInit {
-  bottomNavLinks: { name: string, link: string }[] = [
-    {
-      name: `Home`,
-      link: `#home`
-    },
-    {
-      name: `About Us`,
-      link: `#about-us`
-    },
-    {
-      name: `Services`,
-      link: `#services`
-    },
-    {
-      name: `Annuity Education`,
-      link: `#annuity-education`
-    },
-    {
-      name: `Terms of Use`,
-      link: `#terms-of-use`
-    },
-    {
-      name: `Purchase Annuities`,
-      link: `#purchase-annuities`
-    },
-    {
-      name: `Our Customers`,
-      link: `#out-customers`
-    },
-    {
-      name: `Contact`,
-      link: `#contact`
-    }
-  ]
-
-  bottomSocialLinks: { name: string, link: string, src: string }[] = [
-    {
-      name: `Twitter`,
-      link: `https://twitter.com`,
-      src: ``
-    },
-    {
-      name: `Facebook`,
-      link: `https://facebook.com`,
-      src: ``
-    },
-    {
-      name: `YouTube`,
-      link: `https://youtube.com`,
-      src: ``
-    },
-    {
-      name: `Instagram`,
-      link: `https://instagram.com`,
-      src: ``
-    },
-  ]
 
   constructor() { }
 
