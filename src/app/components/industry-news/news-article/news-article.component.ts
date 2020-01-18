3ace38d0771b76f950c3ae3c64d574a1d06a7acd
Jonathan Sexton <joncsexton@gmail.com>
2020-01-18 17:32:25 -0600

    adjusting industry news section

--- src/app/components/industry-news/news-article/news-article.component.ts
+++ src/app/components/industry-news/news-article/news-article.component.ts
@@ -1,15 +1,55 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit } from "@angular/core";
 
 @Component({
-  selector: 'app-news-article',
-  templateUrl: './news-article.component.html',
-  styleUrls: ['./news-article.component.css']
+  selector: "app-news-article",
+  templateUrl: "./news-article.component.html",
+  styleUrls: ["./news-article.component.css"]
 })
 export class NewsArticleComponent implements OnInit {
+  industryNewsArticles: {
+    title: string;
+    link: string;
+    exerpt: string;
+    imgURL: string;
+  }[] = [
+    {
+      title: `Industry Article 1`,
+      link: `#`,
+      exerpt: `Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.`,
+      imgURL: `#`
+    },
+    {
+      title: `Industry Article 2`,
+      link: `#`,
+      exerpt: `Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.`,
+      imgURL: `#`
+    },
+    {
+      title: `Industry Article 3`,
+      link: `#`,
+      exerpt: `Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.`,
+      imgURL: `#`
+    },
+    {
+      title: `Industry Article 4`,
+      link: `#`,
+      exerpt: `Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.`,
+      imgURL: `#`
+    },
+    {
+      title: `Industry Article 5`,
+      link: `#`,
+      exerpt: `Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.`,
+      imgURL: `#`
+    },
+    {
+      title: `Industry Article 6`,
+      link: `#`,
+      exerpt: `Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.`,
+      imgURL: `#`
+    }
+  ];
+  constructor() {}
 
-  constructor() { }
-
-  ngOnInit() {
-  }
-
+  ngOnInit() {}
 }
