6de3273ef6b62db55acb61ef8e6706e8c64ffc74
Jonathan Sexton <joncsexton@gmail.com>
2020-01-18 22:28:19 -0600

    adjusting properties for responsiveness

--- src/app/components/industry-news/news-article/news-article.component.ts
+++ src/app/components/industry-news/news-article/news-article.component.ts
@@ -11,42 +11,49 @@ export class NewsArticleComponent implements OnInit {
     link: string;
     exerpt: string;
     imgURL: string;
+    alt: string
   }[] = [
     {
       title: `Industry Article 1`,
       link: `#`,
       exerpt: `Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.`,
-      imgURL: `#`
+      imgURL: `../../../../assets/images/christine-roy-ir5MHI6rPg0-unsplash.jpg`,
+      alt: ``
     },
     {
       title: `Industry Article 2`,
       link: `#`,
       exerpt: `Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.`,
-      imgURL: `#`
+      imgURL: `../../../../assets/images/fabian-blank-pElSkGRA2NU-unsplash.jpg`,
+      alt: ``
     },
     {
       title: `Industry Article 3`,
       link: `#`,
       exerpt: `Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.`,
-      imgURL: `#`
+      imgURL: `../../../../assets/images/helloquence-5fNmWej4tAA-unsplash.jpg`,
+      alt: ``
     },
     {
       title: `Industry Article 4`,
       link: `#`,
       exerpt: `Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.`,
-      imgURL: `#`
+      imgURL: `../../../../assets/images/markus-spiske-5gGcn2PRrtc-unsplash.jpg`,
+      alt: ``
     },
     {
       title: `Industry Article 5`,
       link: `#`,
       exerpt: `Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.`,
-      imgURL: `#`
+      imgURL: `../../../../assets/images/michael-longmire-lhltMGdohc8-unsplash.jpg`,
+      alt: ``
     },
     {
       title: `Industry Article 6`,
       link: `#`,
       exerpt: `Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.`,
-      imgURL: `#`
+      imgURL: `../../../../assets/images/thought-catalog-Xeo_7HSwYsA-unsplash.jpg`,
+      alt: ``
     }
   ];
   constructor() {}
