dd8d5ba8c2c3f55b7975d82b6cf44903859abffe
Jonathan Sexton <joncsexton@gmail.com>
2020-01-25 17:34:45 -0600

    issues with customer section need attention

--- src/app/components/customer-section/customer-section.component.ts
+++ src/app/components/customer-section/customer-section.component.ts
@@ -6,6 +6,25 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./customer-section.component.css']
 })
 export class CustomerSectionComponent implements OnInit {
+  customerCards: {name: string, img: string, content: string}[] = [
+    {
+      name: 'Hardol Doe',
+      img: `../../../../assets/images/tyler-nix-ZGa9d1a_4tA-unsplash.jpg`,
+      content: `Swearem ipsum dolor sit amet, consectetur adipiscing elit. Aliquam scelerisque accumsan nisl, a mattis eros vestibulum et. Vestibulum placerat purus ut nibh aliquam fringilla.`
+    },
+    {
+      name: 'Jane Schmidt',
+      img: `../../../../assets/images/marivi-pazos-cvpk5Y4ZWUs-unsplash.jpg`,
+      content: `Swearem ipsum dolor sit amet, consectetur adipiscing elit. Aliquam scelerisque accumsan nisl, a mattis eros vestibulum et. Vestibulum placerat purus ut nibh aliquam fringilla.`
+    },
+    {
+      name: 'John Smith',
+      img: `../../../../assets/images/marius-ciocirlan-vMV6r4VRhJ8-unsplash.jpg`,
+      content: `Swearem ipsum dolor sit amet, consectetur adipiscing elit. Aliquam scelerisque accumsan nisl, a mattis eros vestibulum et. Vestibulum placerat purus ut nibh aliquam fringilla.`
+    },
+
+  ]
+
 
   constructor() { }
 
