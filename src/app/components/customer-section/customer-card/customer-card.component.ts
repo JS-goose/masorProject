4e97b1d2f7dd94b234af8f50fcb6bfce54f0c2bf
Jonathan Sexton <joncsexton@gmail.com>
2020-02-01 07:13:09 -0600

    updated customer section to get it the way I wanted

--- src/app/components/customer-section/customer-card/customer-card.component.ts
+++ src/app/components/customer-section/customer-card/customer-card.component.ts
@@ -7,26 +7,6 @@ import { Customer } from "../customer model/customer.model";
   styleUrls: ["./customer-card.component.css"]
 })
 export class CustomerCardComponent implements OnInit {
-  customerCards: Customer[] = [
-    new Customer(
-      `Hardol Doe`,
-      `../../../../assets/images/tyler-nix-ZGa9d1a_4tA-unsplash.jpg`,
-      `Swearem ipsum dolor sit amet, consectetur adipiscing elit. Aliquam scelerisque accumsan nisl, a mattis eros vestibulum et. Vestibulum placerat purus ut nibh aliquam fringilla.`,
-      `a human smiling`
-    ),
-    new Customer(
-      `Jane Schmidt`,
-      `../../../../assets/images/marivi-pazos-cvpk5Y4ZWUs-unsplash.jpg`,
-      `Swearem ipsum dolor sit amet, consectetur adipiscing elit. Aliquam scelerisque accumsan nisl, a mattis eros vestibulum et. Vestibulum placerat purus ut nibh aliquam fringilla.`,
-      `a human woman`
-    ),
-    new Customer(
-      `John Smith`,
-      `../../../../assets/images/marius-ciocirlan-vMV6r4VRhJ8-unsplash.jpg`,
-      `Swearem ipsum dolor sit amet, consectetur adipiscing elit. Aliquam scelerisque accumsan nisl, a mattis eros vestibulum et. Vestibulum placerat purus ut nibh aliquam fringilla.`,
-      `a man smiling`
-    )
-  ];
   constructor() {}
 
   ngOnInit() {}
--- src/app/components/customer-section/customer-section.component.ts
+++ src/app/components/customer-section/customer-section.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from "@angular/core";
+import { Customer } from "./customer model/customer.model";
 
 @Component({
   selector: "app-customer-section",
@@ -6,6 +7,26 @@ import { Component, OnInit } from "@angular/core";
   styleUrls: ["./customer-section.component.css"]
 })
 export class CustomerSectionComponent implements OnInit {
+  customerCards: Customer[] = [
+    new Customer(
+      `Hardol Doe`,
+      `../../../assets/images/tyler-nix-ZGa9d1a_4tA-unsplash.jpg`,
+      `Swearem ipsum dolor sit amet, consectetur adipiscing elit. Aliquam scelerisque accumsan nisl, a mattis eros vestibulum et. Vestibulum placerat purus ut nibh aliquam fringilla.`,
+      `a human smiling`
+    ),
+    new Customer(
+      `Jane Schmidt`,
+      `../../../assets/images/marivi-pazos-cvpk5Y4ZWUs-unsplash.jpg`,
+      `Swearem ipsum dolor sit amet, consectetur adipiscing elit. Aliquam scelerisque accumsan nisl, a mattis eros vestibulum et. Vestibulum placerat purus ut nibh aliquam fringilla.`,
+      `a human woman`
+    ),
+    new Customer(
+      `John Smith`,
+      `../../../assets/images/marius-ciocirlan-vMV6r4VRhJ8-unsplash.jpg`,
+      `Swearem ipsum dolor sit amet, consectetur adipiscing elit. Aliquam scelerisque accumsan nisl, a mattis eros vestibulum et. Vestibulum placerat purus ut nibh aliquam fringilla.`,
+      `a man smiling`
+    )
+  ];
   constructor() {}
 
   ngOnInit() {}
