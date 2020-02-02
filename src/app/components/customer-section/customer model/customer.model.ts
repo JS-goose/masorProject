f50c72fc99dd2d0fdd7ff52803df5ab6cf44b72f
Jonathan Sexton <joncsexton@gmail.com>
2020-02-02 11:23:33 -0600

    customer section work

--- src/app/components/customer-section/customer model/customer.model.ts
+++ src/app/components/customer-section/customer model/customer.model.ts
@@ -10,11 +10,19 @@ export class Customer {
   public image: string;
   public content: string;
   public imageAlt: string;
+  public imgIndex: string | number;
 
-  constructor(name: string, image: string, content: string, imageAlt: string) {
+  constructor(
+    name: string,
+    image: string,
+    content: string,
+    imageAlt: string,
+    imgIndex: string | number
+  ) {
     this.name = name;
     this.image = image;
     this.content = content;
     this.imageAlt = imageAlt;
+    this.imgIndex = imgIndex;
   }
 }
--- src/app/components/customer-section/customer-section.component.ts
+++ src/app/components/customer-section/customer-section.component.ts
@@ -12,19 +12,43 @@ export class CustomerSectionComponent implements OnInit {
       `Hardol Doe`,
       `../../../assets/images/tyler-nix-ZGa9d1a_4tA-unsplash.jpg`,
       `Swearem ipsum dolor sit amet, consectetur adipiscing elit. Aliquam scelerisque accumsan nisl, a mattis eros vestibulum et. Vestibulum placerat purus ut nibh aliquam fringilla.`,
-      `a human smiling`
+      `a human smiling`,
+      ``
+    ),
+    new Customer(
+      `Hardol Doe`,
+      `../../../assets/images/tyler-nix-ZGa9d1a_4tA-unsplash.jpg`,
+      `Swearem ipsum dolor sit amet, consectetur adipiscing elit. Aliquam scelerisque accumsan nisl, a mattis eros vestibulum et. Vestibulum placerat purus ut nibh aliquam fringilla.`,
+      `a human smiling`,
+      ``
     ),
     new Customer(
       `Jane Schmidt`,
       `../../../assets/images/marivi-pazos-cvpk5Y4ZWUs-unsplash.jpg`,
       `Swearem ipsum dolor sit amet, consectetur adipiscing elit. Aliquam scelerisque accumsan nisl, a mattis eros vestibulum et. Vestibulum placerat purus ut nibh aliquam fringilla.`,
-      `a human woman`
+      `a human woman`,
+      ``
+    ),
+    new Customer(
+      `Jane Schmidt`,
+      `../../../assets/images/marivi-pazos-cvpk5Y4ZWUs-unsplash.jpg`,
+      `Swearem ipsum dolor sit amet, consectetur adipiscing elit. Aliquam scelerisque accumsan nisl, a mattis eros vestibulum et. Vestibulum placerat purus ut nibh aliquam fringilla.`,
+      `a human woman`,
+      ``
     ),
     new Customer(
       `John Smith`,
       `../../../assets/images/marius-ciocirlan-vMV6r4VRhJ8-unsplash.jpg`,
       `Swearem ipsum dolor sit amet, consectetur adipiscing elit. Aliquam scelerisque accumsan nisl, a mattis eros vestibulum et. Vestibulum placerat purus ut nibh aliquam fringilla.`,
-      `a man smiling`
+      `a man smiling`,
+      ``
+    ),
+    new Customer(
+      `John Smith`,
+      `../../../assets/images/marius-ciocirlan-vMV6r4VRhJ8-unsplash.jpg`,
+      `Swearem ipsum dolor sit amet, consectetur adipiscing elit. Aliquam scelerisque accumsan nisl, a mattis eros vestibulum et. Vestibulum placerat purus ut nibh aliquam fringilla.`,
+      `a man smiling`,
+      ``
     )
   ];
   constructor() {}
