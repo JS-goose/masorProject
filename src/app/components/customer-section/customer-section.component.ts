7851e4cf0a46756245d780e41bb112893d0f669c
Jonathan Sexton <joncsexton@.com>
2020-02-20 05:37:55 -0600

    trying to fix production build issues

--- src/app/components/customer-section/customer-section.component.ts
+++ src/app/components/customer-section/customer-section.component.ts
@@ -4,7 +4,7 @@ import { Customer } from "./customer model/customer.model";
 @Component({
   selector: "app-customer-section",
   templateUrl: "./customer-section.component.html",
-  styleUrls: ["./customer-section.component.css"]
+  styleUrls: ["./customer-section.component.css"],
 })
 export class CustomerSectionComponent implements OnInit {
   customerCards: Customer[] = [
@@ -13,21 +13,21 @@ export class CustomerSectionComponent implements OnInit {
       `../../../assets/images/dragos-gontariu-GH-mSApoKO0-unsplash.jpg`,
       `Swearem ipsum dolor sit amet, consectetur adipiscing elit. `,
       `a human smiling`,
-      ``
+      `1`
     ),
     new Customer(
       `Jane Schmidt`,
       `../../../assets/images/marivi-pazos-cvpk5Y4ZWUs-unsplash.jpg`,
       `Aliquam scelerisque accumsan nisl, a mattis eros vestibulum et. `,
       `a human woman`,
-      ``
+      `2`
     ),
     new Customer(
       `John Smith`,
       `../../../assets/images/marius-ciocirlan-vMV6r4VRhJ8-unsplash.jpg`,
       `Vestibulum placerat purus ut nibh aliquam fringilla.`,
       `a man smiling`,
-      ``
+      `3`
     ),
   ];
   constructor() { }
