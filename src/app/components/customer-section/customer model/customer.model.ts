311997c99a8da2cb83ad0d0602eec578af880e40
Jonathan Sexton <joncsexton@.com>
2020-02-20 05:47:41 -0600

    adjusted some code

--- src/app/components/customer-section/customer model/customer.model.ts
+++ src/app/components/customer-section/customer model/customer.model.ts
@@ -1,10 +1,10 @@
-import { CommonModule } from "@angular/common";
-import { NgModule } from "@angular/core";
+// import { CommonModule } from "@angular/common";
+// import { NgModule } from "@angular/core";
 
-@NgModule({
-  declarations: [],
-  imports: [CommonModule]
-})
+// @NgModule({
+//   declarations: [],
+//   imports: [CommonModule]
+// })
 export class Customer {
   public name: string;
   public image: string;
